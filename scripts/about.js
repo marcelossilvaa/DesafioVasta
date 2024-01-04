09b0ece4c197ecf00e24e3e36281473140d127a9
Marcelo Silva <marcelo_810@hotmail.com>
2024-01-03 23:29:42 -0300

    feat/scroll animation

--- scripts/about.js
+++ scripts/about.js
@@ -1,3 +1,14 @@
 document.getElementById('readmore-button').addEventListener('click', function() {
   document.getElementById('hidden-content').style.display = 'block';
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Seleciona o elemento principal da seção "about"
+  var aboutContainer = document.querySelector('.about--container');
+
+  // Adiciona a classe 'fade-in' após um pequeno atraso
+  setTimeout(function() {
+      aboutContainer.classList.add('fade-in');
+  }, 500); // Ajuste o valor do atraso conforme necessário
+});
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,24 +1,66 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const loadMoreButton = document.getElementById('loadMore');
-  const portfolioImages = Array.from(document.getElementsByClassName('portfolio--image'));
+const loadMoreButton = document.getElementById('loadMore');
+const portfolioImages = Array.from(document.getElementsByClassName('portfolio--image'));
 
-  function checkWidth() {
-      const windowWidth = window.innerWidth;
-      if (windowWidth <= 768) {
-          portfolioImages.slice(0, 4).forEach(img => img.classList.remove('hidden'));
-          portfolioImages.slice(4).forEach(img => img.classList.add('hidden'));
-          loadMoreButton.style.display = 'block';
-      } else {
-          portfolioImages.forEach(img => img.classList.remove('hidden'));
-          loadMoreButton.style.display = 'none';
-      }
-  }
+function checkWidth() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 768) {
+        portfolioImages.slice(0, 4).forEach(img => img.classList.remove('hidden'));
+        portfolioImages.slice(4).forEach(img => img.classList.add('hidden'));
+        loadMoreButton.style.display = 'block';
+    } else {
+        portfolioImages.forEach(img => img.classList.remove('hidden'));
+        loadMoreButton.style.display = 'none';
+    }
+}
 
-  loadMoreButton.addEventListener('click', () => {
-      portfolioImages.forEach(img => img.classList.remove('hidden'));
-      loadMoreButton.style.display = 'none';
-  });
+loadMoreButton.addEventListener('click', () => {
+    portfolioImages.forEach(img => img.classList.remove('hidden'));
+    loadMoreButton.style.display = 'none';
+});
 
-  window.addEventListener('resize', checkWidth);
-  checkWidth();
+window.addEventListener('resize', checkWidth);
+checkWidth();
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var aboutContainer = document.querySelector('.about--container');
+    var portfolioContent = document.getElementById('portfolioContent');
+    var portfolioImages = document.querySelectorAll('.portfolio--image');
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+
+    function addFadeInClass(element) {
+        element.classList.add('fade-in');
+    }
+    function checkVisibilityAndFadeIn() {
+        if (isElementInViewport(aboutContainer)) {
+            addFadeInClass(aboutContainer);
+        }
+
+        if (isElementInViewport(portfolioContent)) {
+            addFadeInClass(portfolioContent);
+        }
+
+        portfolioImages.forEach(function(image) {
+            if (isElementInViewport(image)) {
+                addFadeInClass(image);
+            }
+        });
+    }
+    checkVisibilityAndFadeIn();
+    window.addEventListener('scroll', function() {
+        checkVisibilityAndFadeIn();
+    });
 });
